372b811e6dd491a1f2275b36990ad8e7b90f253f
gforts <45782523+gforts@users.noreply.github.com>
2018-12-12 14:52:04 +0700

    Add files via upload

---
+++ project1/README.md
@@ -0,0 +1,3 @@
+# project1
+# This is a sample README.md file
+#It contains the project1 description
---
+++ project1/Sample.java
@@ -0,0 +1,9 @@
+/* Here code starts*/
+public class Sample{
+	public stativ void maain(String args[]){
+		System.out.println("This is just a sample java file for Git");
+		System.out.println("We are adding the output lines to see the changes.");
+System.out.println("This is second modification in java file to see the staged and unstaged changes.");
+	}
+		}
+/* Here code ends*/
